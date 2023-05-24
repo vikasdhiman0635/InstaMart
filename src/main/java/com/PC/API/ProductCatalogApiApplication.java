2d20d35ba73fbd2e4e7a8b47515399f5b04a49c4
vikasdhiman0635 <vikasdhiman@gmail.com>
2023-05-24 23:43:18 +0530

    Product_Catalog_ini

---
+++ src/main/java/com/PC/API/ProductCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.PC.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PC/API/ProductCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PC.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
